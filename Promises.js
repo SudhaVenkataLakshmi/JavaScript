78a3b58b95f1165ecc659fc6195bdc04add73545
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-02-19 19:38:13 +0530

    Promises example

--- Promises.js
+++ Promises.js
@@ -1,16 +1,29 @@
-let promise = new Promise(function(resolve, reject){
-    let success = true;
-    if(success){
-        resolve("Operation SuccessFul!");
-    }else{
-        reject("Operation Failure!");
-    }
-});
+// let promise = new Promise(function(resolve, reject){
+//     let success = true;
+//     if(success){
+//         resolve("Operation SuccessFul!");
+//     }else{
+//         reject("Operation Failure!");
+//     }
+// });
 
-promise
-    .then(function(result){
-        console.log(result);
-    })
-    .catch(function(error){
-        console.log(error);
+// promise
+//     .then(function(result){
+//         console.log(result);
+//     })
+//     .catch(function(error){
+//         console.log(error);
+//     });
+
+
+
+function fetchData() {
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve("Data Received");
+        },2000);
     });
+}
+fetchData().then(function(data){
+    console.log(data);
+});
